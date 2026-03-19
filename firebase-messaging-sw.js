5942de8525a0437025a8636d9485d7d638ab15c6
EHAB1221a <ea109754@gmail.com>
2026-03-19 11:56:20 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/12.8.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.8.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyAjpqfDdoxYs8rRBw4UKOrvtMoqBDB0Z_s",
@@ -10,3 +10,16 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+// هذه الخطوة هي التي تجعل الإشعار يظهر والموقع مغلق
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png' // يمكنك وضع رابط أيقونة هنا
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
